5c510c226a4fbe46fb8169140abfb95372382337
bartek 'basz' szurgot <git@baszerr.eu>
2017-10-28 23:29:07 +0200

    added automatic line-wrapping for code listings

---
+++ cpp/long_lines-m.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "this line is far too long to fit on the screen";
+  std::cout << "so we break it into multiple lines automatically";
+  std::cout << "and we're good! :)" << std::endl;
+}
